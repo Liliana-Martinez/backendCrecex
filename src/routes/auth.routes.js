c6f2d22f3b815316d7ff5fd9d13dd9d44e345a02
Liliana-Martinez <lilia001125@gmail.com>
2025-04-09 08:34:56 -0600

    updated files

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,41 +2,39 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const moment = require('moment');
+//const moment = require('moment');
 const authController = require('../controllers/auth.controller');
 const middleware = require('./middleware');
 
-
-router.use(middleware.checkToken);
-//Definición de las rutas
-/*Ruta para obtener los usuarios y comprobar la BD
-router.get('/usuarios', async (req, res) => {
-    try {
-        const users = await authController.getAll();
-        res.json(users);
-    } catch (error) {
-        res.status(500).json({ error: "Error al obtener los usuarios", details: error.message });
-    }
-});*/
-
-
 //Ruta para el registro
 router.post('/register', async (req, res) => {
-    console.log(req.body);
-    req.body.password = bcrypt.hashSync(req.body.password, 10);
-    const result = await authController.insert(req.body);
-    res.json(result);
+    const { user, password } = req.body;
+
+    try {
+        //Verificar si el usuario ya existe
+        const existingUSer = await authController.getUser(user);
+        if (existingUSer) {
+            return res.status(400).json({ error: 'El usuario ya existe' });
+        }
+
+        req.body.password = bcrypt.hashSync(req.body.password, 10);
+        const result = await authController.insert(req.body);
+        res.json(result);
+    } catch (error) {
+        console.error('Error en el registro', error);
+        res.status(500).json({ error: 'Ocurrio un error al registrar el usuario' });
+    }
 });
 
-//Funcion para el login
+//Ruta para el login
 router.post('/login', async (req, res) => {
     const user = await authController.getUser(req.body.user)
     if (user === undefined) {
-        res.json({ error: 'Error, usuario o contraseña incorrectos'})
+        res.status(401).json({ error: 'Error, usuario o contraseña incorrectos'})
     } else {
         const equals = bcrypt.compareSync(req.body.password, user.password);
         if (!equals) {
-            res.json({error: 'Error, usuario o contraseña incorrectos'});
+            res.status(401).json({error: 'Error, usuario o contraseña incorrectos'});
         } else {
             res.json({
                 succesfull: createToken(user),
@@ -46,7 +44,7 @@ router.post('/login', async (req, res) => {
     }
 });
 
-//Función para generar el token
+/*Función para generar el token*/
 const createToken = (user) => {
     let payload = {
         userId: user.id,
@@ -55,7 +53,9 @@ const createToken = (user) => {
     return jwt.sign(payload, process.env.TOKEN_KEY, { expiresIn: '1d' }); // Expira en 1 día
 };
 
-//Manejador de ruta que recibira el id del usuario en el header gracias al middleware
+router.use(middleware.checkToken);
+
+/*Manejador de ruta que recibira el id del usuario en el header gracias al middleware*/
 router.get('/mainUser', (req, res) => {
     authController.getById(req.userId)
     .then(rows => {
@@ -63,5 +63,6 @@ router.get('/mainUser', (req, res) => {
     })
     .catch(err => console.log(err));
 });
+
 
 module.exports = router;
