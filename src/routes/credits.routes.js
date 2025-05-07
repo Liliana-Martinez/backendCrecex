a651c8def51013c1d6290c718aadf754b1b62a18
ClaudiaRafaelSantos <clauyanethrs@gmail.com>
2025-05-07 14:37:57 -0600

    Update credits.routes.js
    search

--- src/routes/credits.routes.js
+++ src/routes/credits.routes.js
@@ -3,43 +3,5 @@ const router = express.Router();
 const creditsController = require('../controllers/credits.controller');
 const middleware = require('./middleware');
 router.post('/new', creditsController.createNewCredit);
-<<<<<<< HEAD
-=======
 
-router.post('/buscar-cliente', async (req, res) => {
-    try {
-        const { nombreCompleto, modulo } = req.body;
-
-        console.log('Nombre recibido:', nombreCompleto);
-        console.log('Módulo solicitado:', modulo);
-
-        let result;
-
-        switch (modulo) {
-            case 'new':
-            case 'renew':
-            case 'additional':
-                result = await creditsController.SearchCredit(nombreCompleto);
-                break;
-            case 'collectors':
-                result = await creditsController.SearchCollectors(nombreCompleto);
-                break;
-            case 'consult':
-                result = await creditsController.searchConsult(nombreCompleto);
-                break;
-            case 'modify':
-                result = await creditsController.SearchModify(nombreCompleto);
-                break;
-            default:
-                return res.status(400).json({ message: 'Módulo no reconocido' });
-        }
-
-        res.status(200).json(result);
-    } catch (error) {
-        console.error('Error al procesar la solicitud:', error);
-
-        res.status(500).json({ message: 'Error en el servidor' });
-    }
-});
->>>>>>> 31be0ef7d289aad1b5a10b0f2fc666725b3d0fad
 module.exports = router;
