e0c17b1f292206f6cff4552a3bd1bf26b39456f4
ClaudiaRafaelSantos <clauyanethrs@gmail.com>
2025-05-08 14:05:04 -0600

    collectors

--- src/routes/credits.routes.js
+++ src/routes/credits.routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const middleware = require('./middleware');
+const creditsController = require('../controllers/credits.controller');
 router.post('/new', creditsController.createNewCredit);
 
 module.exports = router;
