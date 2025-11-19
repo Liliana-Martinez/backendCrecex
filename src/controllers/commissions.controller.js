9a10629d8de8a4aa0cb166e17243dcee5f4b9909
Liliana Martinez <lilia001125@gmail.com>
2025-11-19 09:36:51 -0600

    Columna extras casi terminada

--- src/controllers/commissions.controller.js
+++ src/controllers/commissions.controller.js
@@ -25,14 +25,14 @@ function queryAsync(sql, params = []) {
 const getCommissionesByZone = async (idZona) => {
   const collectionRate = await getCollectionRate(idZona);//Columna porcentaje de cobranza
   const collectionExpenses = await getCollectionExpenses(idZona);//Columna gastos de cobranza
-  /*const numberOfCredits = await getNumberOfCredits(idZona); //Numero de creditos
+  const numberCredits = await getTotalCredits(idZona); //Numero de creditos
   const extras = await getExtras(idZona); //Extras*/
 
   return {
     collectionRate,
     collectionExpenses,
-    /*numberOfCredits,
-    extras*/
+    numberCredits,
+    extras
   };
 }
 
@@ -78,8 +78,7 @@ async function getCollectionRate(idZona) {
     console.log('amountPaid: ', amountPaid);
     console.log('extras: ', extras);
 
-    //comparar el resultado de "cantidad" con "cantidadPag
-    // ada" para obtener los porcentajes
+    //comparar el resultado de "cantidad" con "cantidadPagada" para obtener los porcentajes
     if (totalPaid >= total) { //Es decir, 100% o más
       collectionRate = (totalPaid * 8) / 100;
     } else {
@@ -154,9 +153,138 @@ async function getCollectionExpenses(idZona) {
   return collectionExpenses;
 }
 
+//Funcion para obtener la cantidad de creditos que hubo por semana por zona
+async function getTotalCredits(idZona) {
+  const numberCredits = `
+    SELECT COUNT(*) AS totalCreditos
+    FROM creditos c
+    INNER JOIN clientes cl ON c.idCliente = cl.idCliente
+    WHERE
+      c.estado = 'activo'
+      AND cl.idZona = ?
+      AND c.fechaEntrega BETWEEN ? AND ?`;
+
+
+  const resultNumberCredits = await queryAsync(numberCredits, [idZona, startDate, endDate]);
+  const totalCredits = (resultNumberCredits[0]?.totalCreditos || 0) * 100;
+  console.log('totalCredits: ', totalCredits);
+
+  return totalCredits;
+}
+
+//Funcion para obtener los extras
+async function getExtras(idZona) {
+
+  //Obtener los supervisores (no repetidos y no nulos) para guardarlos en una lista
+  const supervisorsListQuery = `
+    SELECT DISTINCT supervicion 
+    FROM zonas
+    WHERE supervicion IS NOT NULL AND supervicion != ''`;
+  const supervisorsListResult = await queryAsync(supervisorsListQuery);
+
+  //Crear la lista con los supervisores
+  let supervisorsList = supervisorsListResult.map(row => row.supervicion);
+  console.log('Lista de supervisores: ', supervisorsList);
+
+  //Consulta para obtener la promotora y la supervicion respecto al idZona 
+  const staffQuery = `
+    SELECT promotora, supervicion
+    FROM zonas
+    WHERE 
+      idZona = ?`;
+  
+  const staffQueryResult = await queryAsync(staffQuery, [idZona]);
+  console.log(staffQueryResult);
+  if (staffQueryResult.length > 0) {
+    let promoter = staffQueryResult[0].promotora || null;
+    let supervicion = staffQueryResult[0].supervicion || null;
+    if (supervisorsList.includes(promoter)) {
+      //1. Obtener los IDs de las zonas que le pertenecen al (la) supervisor(a)
+      const supervisorZonesQuery = `SELECT idZona FROM zonas WHERE supervicion = ?`;
+      const supervisorZonesResult = await queryAsync(supervisorZonesQuery, [promoter]);
+
+      let expectedByZone = 0; 
+      let paidByZone = 0;
+      let totalGeneral = 0;
+      let supervisionCommission = 0;
+
+      //Recorrer cada zona del supervisor(a)
+      for (const zone of supervisorZonesResult) {
+        const zoneId = zone.idZona;
+
+        console.log('Zona en proceso: ', zoneId);
+
+        //Consulta para obtener el dinero total que se debe de dar en la semana
+        const expectedMoneyQuery = `
+          SELECT SUM(cantidad) as totalCantidad
+          FROM pagos p
+          INNER JOIN creditos c ON p.idCredito = c.idCredito
+          INNER JOIN clientes cl ON c.idCliente = cl.idCliente
+          WHERE
+            c.estado = 'activo'
+            AND cl.idZona = ?
+            AND fechaEsperada BETWEEN ? AND ?`;
+        
+        const expectedMoneyResult = await queryAsync(expectedMoneyQuery, [zoneId, startDate, endDate]);
+        const expectedTotal = expectedMoneyResult[0].totalCantidad || 0;
+
+        expectedByZone += expectedTotal;
+
+        //Consulta para obtener el dinero pagado realmente
+        const moneyPaidQuery = `
+          SELECT SUM(cantidadPagada) as totalPagado,
+          SUM(extras) as totalExtras
+          FROM pagos p
+          INNER JOIN creditos c ON p.idCredito = c.idCredito
+          INNER JOIN clientes cl ON c.idCliente = cl.idCliente
+          WHERE
+            c.estado = 'activo'
+            AND cl.idZona = ?
+            AND p.fechaPagada BETWEEN ? AND ?
+            AND p.estado IN ('pagado', 'incompleto', 'pagadoAtrasado', 'atraso')`;
+
+        const moneyPaidResult = await queryAsync(moneyPaidQuery, [zoneId, startDate, endDate]);
+        const totalPaid = moneyPaidResult[0]?.totalPagado || 0;
+        const totalExtras = moneyPaidResult[0]?.totalExtras || 0;
+        totalGeneral = totalPaid + totalExtras;
+        paidByZone += totalGeneral;
+
+        //Calcular la comision del supervisor(a)
+        if (paidByZone >= expectedByZone) { //Es decir 100% o mas
+          supervisionCommission = (paidByZone * 6) / 100;
+        } else {
+          //Saber primero el porcentaje que se entrego
+          const supervisionPercentage = ((paidByZone * 100) / expectedByZone).toFixed(2);
+          //Una vez conocido el porcentaje asignar la comision correspondiente
+          if (supervisionPercentage >= 95 && supervisionPercentage <= 99) {
+            supervisionCommission = (paidByZone * 5) / 100;
+          }  else if (supervisionPercentage >= 90 && supervisionPercentage <= 94) {
+            supervisionCommission = (paidByZone * 4) / 100;
+          } else if (supervisionPercentage >= 85 && supervisionPercentage <= 89) {
+            supervisionCommission = (paidByZone * 3) / 100;
+          } else if (supervisionPercentage >= 80 && supervisionPercentage <= 84) {
+            supervisionCommission = (paidByZone * 2) / 100;
+          } else if (supervisionPercentage >= 75 && supervisionPercentage <= 79) {
+            supervisionCommission = (paidByZone * 1) / 100;
+          } else {
+            supervisionCommission = 0;
+          }
+        }
+
+        return supervisionCommission;
+
+      }
+    } else {
+      console.log('La promotora NO es supervisora.');
+    }
+  } else {
+    console.log('No hay registros para esta zona.')
+  }
+  
+
+}
+
 module.exports = { 
   getCommissionesByZone
-  //getCreditsWeekByZone
-  //getCollectionExpenses
 };
 
